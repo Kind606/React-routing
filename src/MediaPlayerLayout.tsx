3ed0e66cc4f77bbe5c2941aff9081f2d490382ce
Kind606 <kevin.hellgren@hotmail.com>
2025-02-18 11:14:43 +0100

    fist commit

---
+++ src/MediaPlayerLayout.tsx
@@ -0,0 +1,14 @@
+export default function MediaPlayerLayout() {
+  return (
+    <div className="flex flex-col text-slate-50">
+      <header className="px-4 py-2 flex justify-between bg-slate-950">
+        <h1>Header</h1>
+        <span>Right</span>
+      </header>
+      <div className="flex gap-2">
+        <aside className="bg-slate-800 p-4 ">SideBar</aside>
+        <main className="bg-slate-900  p-4 rounded"></main>
+      </div>
+    </div>
+  );
+}
